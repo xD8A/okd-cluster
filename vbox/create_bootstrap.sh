3ed25ac8f191d70447c96e92e6571188092cc76b
Dmitry Antipin <antipin.dmitry@gmail.com>
2021-01-02 02:23:12 +0400

    Add scripts for bash (linux)

---
+++ vbox/create_bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VM=okd-bootstrap
+
+CPU_COUNT=1
+RAM_SIZE=4096
+DISK_SIZE=20480
+
+INTNET_NAME=intnet
+
+DISK_PATH="$VBOX_USER_HOME/$VM/$VM.vdi"
+
+if [ ! -d "VBOX_USER_HOME" ]; then
+  echo "Virtual Box home directory does not exist: VBOX_USER_HOME" 1>&2
+  exit 3
+fi
+
+# Create machine
+VBoxManage createvm --register --basefolder "$VBOX_USER_HOME" --name $VM --ostype Fedora_64
+# Setup system resources
+VBoxManage modifyvm $VM --ioapic on
+VBoxManage modifyvm $VM --cpus $CPU_COUNT
+VBoxManage modifyvm $VM --memory $RAM_SIZE
+# Setup network
+VBoxManage modifyvm $VM --nic1 intnet --intnet1 $INTNET_NAME --macaddress1 080027180200
+# Create disk 
+VBoxManage createmedium --filename "$DISK_PATH" --size $DISK_SIZE
+VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$DISK_PATH"
+# Setup boot order
+VBoxManage modifyvm $VM --boot1 dvd --boot2 net --boot3 disk --boot4 none
---
+++ vbox/create_hub.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+VM=okd-hub
+
+CPU_COUNT=1
+RAM_SIZE=4096
+DISK_SIZE=81920
+
+INTNET_NAME=intnet
+VBOXNET_NAME=vboxnet0
+
+ISO_PATH=/mnt/data/Repo/centos/7/isos/CentOS-7-x86_64-DVD-2009.iso
+DISK_PATH="VBOX_USER_HOME/$VM/$VM.vdi"
+KS_PATH=$(realpath ../unattended/hub_ks.cfg)
+
+if [ ! -d "VBOX_USER_HOME" ]; then
+  echo "Virtual Box home directory does not exist: $VBOX_HOME" 1>&2
+  exit 3
+fi
+if [ ! -f "$ISO_PATH" ]; then
+  echo "Installation iso not found: $ISO_PATH" 1>&2
+  exit 3
+fi
+if [ ! -f "$KS_PATH" ]; then
+  echo "Kickstart template not found: $KS_PATH" 1>&2
+  exit 3
+fi
+
+
+# Create machine
+VBoxManage createvm --register --basefolder "$VBOX_USER_HOME" --name $VM --ostype RedHat_64
+# Setup system resources
+VBoxManage modifyvm $VM --ioapic on
+VBoxManage modifyvm $VM --cpus $CPU_COUNT
+VBoxManage modifyvm $VM --memory $RAM_SIZE
+# Setup network
+VBoxManage modifyvm $VM --nic1 intnet --intnet1 $INTNET_NAME --macaddress1 080027180001
+VBoxManage modifyvm $VM --nic2 nat
+VBoxManage modifyvm $VM --nic3 hostonly --hostonlyadapter3 $VBOXNET_NAME
+# Create disk 
+VBoxManage createmedium --filename "$DISK_PATH" --size $DISK_SIZE
+VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$DISK_PATH"
+# Mount ISO image
+VBoxManage storagectl $VM --name "IDE Controller" --add ide --controller PIIX4
+VBoxManage storageattach $VM --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$ISO_PATH"
+# Setup boot order
+VBoxManage modifyvm $VM --boot1 disk --boot2 dvd --boot3 none --boot4 none
+# Unattended install
+VBoxManage unattended install $VM --iso="$ISO_PATH" --start-vm=gui --script-template="$KS_PATH"
